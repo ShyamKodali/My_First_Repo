88f024ee9cb84b7782ac8a8e2573b99f2b6fa707
ShyamKodali <104982929+ShyamKodali@users.noreply.github.com>
2022-09-05 18:04:49 +1000

    Passing Arguments in shell script

---
+++ Shell_Scripting_POC/passing_arguments.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+#Passing Arguments 
+echo $0 $1 $2 $3 ' > echo $1 $2 $3'
+
+#Passing it an array 
+args=("$@")
+echo ${args[0]} ${args[1]} ${args[2]} ${args[3]} 
+
+#Print all the arguments at once
+#Default Variable "@"
+echo $@
+
+#Print Number of arguments that are passed
+echo $#
+
+
