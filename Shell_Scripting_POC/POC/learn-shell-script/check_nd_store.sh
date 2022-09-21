696b7033412b3bc8db05980ab7f864868cb80c53
ShyamKodali <shyamkumarkodali@gmail.com>
2022-09-22 09:12:12 +1000

    .,

--- Shell_Scripting_POC/POC/learn-shell-script/check_nd_store.sh
+++ Shell_Scripting_POC/POC/learn-shell-script/check_nd_store.sh
@@ -1,7 +1,11 @@
 #! /bin/bash  
 
-search_dir=/the/path/to/base/dir/
+search_dir=/Users/kodalisimac/Documents/GitHub/My_First_Repo/Shell_Scripting_POC/POC/learn-shell-script/
 for entry in `ls $search_dir`; 
-do
+do 
     echo $entry
 done
+
+# > = The o/p will be redirected to the contents-of-folder.txt file. If the file already exists, it gets overwritten.
+
+ls > contents-of-folder.txt
