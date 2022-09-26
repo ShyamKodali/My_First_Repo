1834ea0d71d2a05cd0353266331127bf7e6c996a
ShyamKodali <shyamkumarkodali@gmail.com>
2022-09-27 01:14:00 +1000

    3

--- Shell_Scripting_POC/POC/learn-shell-script/print_docker_version.sh
+++ Shell_Scripting_POC/POC/learn-shell-script/print_docker_version.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-which docker
 
-if [ $? -eq 0 ]
-then
-    docker --version | grep "Docker version"
-    if [ $? -eq 0 ]
-    then
-        echo "docker existing"
-    else
-        echo "install docker"
-    fi
-else
-    echo "install docker" >&2
-fi
+docker --version 
+
+echo "******************* Expected Output is just a numerical value of docker version using awk command *******************"
+
+docker --version | awk '{print $3}'
