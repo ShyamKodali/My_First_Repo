6c0722b687621dfc3e3992502a6eac0c6a360ad5
ShyamKodali <shyamkumarkodali@gmail.com>
2022-09-27 00:55:04 +1000

    2

--- Shell_Scripting_POC/POC/learn-shell-script/list_nd_check_perm.sh
+++ Shell_Scripting_POC/POC/learn-shell-script/list_nd_check_perm.sh
@@ -3,10 +3,15 @@
 #2a 
 
 search_dir=/Users/kodalisimac/Documents/GitHub/My_First_Repo/Shell_Scripting_POC/POC/learn-shell-script/
-for entry in `ls $search_dir`; 
-do 
-    echo $entry
-done
+file * $search_dir
+echo "********************Listing Only Python Files*****************************"
+ls $search_path | grep *.py
+
 
 #2b 
 
+search_dir_2b=/Users/kodalisimac/Documents/GitHub/My_First_Repo/Shell_Scripting_POC/POC/learn-shell-script/file2.txt
+for entry_2b in `ls -la $search_dir_2b`; 
+do 
+    echo $entry_2b
+done
